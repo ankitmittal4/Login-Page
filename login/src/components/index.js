e39784335e3c7033ae9b827bcac523547989945a
Ankit <ankitmittal0814@gmail.com>
2024-01-06 22:44:41 +0530

    Create login

---
+++ login/src/components/index.js
@@ -0,0 +1,4 @@
+import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
+export { Logo, Button, Input };
--- login/src/main.jsx
+++ login/src/main.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import { AuthLayout } from "./components/index.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import Signup from "./pages/Signup";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
